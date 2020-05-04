e3dbc64f222daf0d92572e707fca32477b24e2ea
ChevalGlass <darkhand16@gmail.com>
2020-05-04 00:52:34 -0700

    Update replace_between.py
    Added another function with a more concise regex match.

--- python/replace_between.py
+++ python/replace_between.py
@@ -11,3 +11,13 @@ def replace_between(start, end, file, repl):
 	# This needs to protect against files that do not exist!
 	with open(file, "w") as open_file:
 		open_file.write(whole_file)
+
+# v2 with a more consice regex.
+#'(?<='+start+')[\s\S]*(?='+end+')'
+def replace_between2(start, end, file, repl):
+	whole_file = ''
+	with open(file, 'r') as open_file:
+		whole_file = re.sub(r'(?<='+start+')[\\s\\S]*(?='+end+')', repl, open_file.read())
+
+	with open(file, "w") as open_file:
+		open_file.write(whole_file)
